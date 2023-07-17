96c05c95bc7bf9ae6212468f1ac3a966f051a899
yuna <elldia1222@gmail.com>
2023-07-17 22:54:13 +0900

    diary img upload

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import './styles/diary.css';
+import Util from './util';
+// import {getEmotionImgById} from './util'
 
 function App() {
   return (
     <>
-      
+      {
+        <Util />
+      /* <img src={getEmotionImgById(1)} alt="" />    
+      <img src={getEmotionImgById(2)} alt="" />    
+      <img src={getEmotionImgById(3)} alt="" />    
+      <img src={getEmotionImgById(4)} alt="" />    
+      <img src={getEmotionImgById(5)} alt="" /> */}    
     </>
   );
 }
